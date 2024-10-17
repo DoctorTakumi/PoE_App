1514342d494be905ffb4eb40b363ebd13cf45a48
DoctorTakumi <ivan.bonacic12@gmail.com>
2024-10-17 19:05:55 +0200

    Updating code hierarchy
    Updating README
    renaming modules
    cleaning the code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PoE_App
  PoE app for leveling
+
+ **TO DO**
+ Write a loop after answer is given so the program can ask the user if there is anything else to help with
+ Add notes in certain places (maybe after quest_input): for more info, check...related poe URL
+ 
+ **Ideas to split the app (for now):**
+ 1. Quest skipper
+ 2. Quest helper
+ 3. When to relog to skip animations
--- poe_app/main.py
+++ poe_app/main.py
@@ -1,47 +1,47 @@
-import quests as q
-import func as f
+from utils import quests_module
+from utils import user_inputs
 
 print ("Welcome to the POE speed-leveling guide!")
             
 # calling the act function
-act_input = f.get_valid_act()
+act_input = user_inputs.get_valid_act()
 
 # Act 1 decision making tree
 # If the user is in Act 1, ask for the quest
 if act_input == 1:
     print ("Here is the list of Act 1 quests:")
-    for key, value in q.quests_act1.items():
+    for key, value in quests_module.quests_act1.items():
         print (f"{key}: {value}")
         
     # calling the quest function
-    quest_input = f.get_valid_quest_input(1, 11)
+    quest_input = user_inputs.get_valid_quest_input(1, 11)
     
     # Check which quest the user selected and respond accordingly
     if quest_input == 4:
-        print(f"{q.quests_act1[quest_input]} is optional quest, you can go on!")
+        print(f"{quests_module.quests_act1[quest_input]} is optional quest, you can go on!")
         print ("Reward for the quest is Book of Regrets.")
     elif quest_input == 11:
         # calling the char_check function
-        char_check = f.get_valid_char_check() # use the validated input
+        char_check = user_inputs.get_valid_char_check() # use the validated input
         if char_check == "yes" or char_check == "y":
             print ("You have to do it for the ascendancy!")
         else:
             print("You can skip it!")
     else:
-        print(f"You have to do {q.quests_act1[quest_input]}.")
+        print(f"You have to do {quests_module.quests_act1[quest_input]}.")
         
 # Act 2 decision making tree
 # If the user is in Act 2, ask for the quest
 elif act_input == 2:
     print ("\nHere is the list of Act 2 quests:")
-    for key, value in q.quests_act2.items():
+    for key, value in quests_module.quests_act2.items():
         print (f"{key}: {value}")
         
     # calling the quest function
-    quest_input = f.get_valid_quest_input(1, 14)
+    quest_input = user_inputs.get_valid_quest_input(1, 14)
     
     if quest_input == 1 or quest_input == 2 or quest_input == 3:
-        print (f"{q.quests_act2[quest_input]} is optional quest, you can go on!")
+        print (f"{quests_module.quests_act2[quest_input]} is optional quest, you can go on!")
         # additional if/elif block for quest rewards
         if quest_input == 1:
             print ("Reward for the quest is Flask or Belt of your choice.")
@@ -52,57 +52,17 @@ elif act_input == 2:
     
     elif quest_input == 14:
         # calling the char_check function
-        char_check = f.get_valid_char_check()
+        char_check = user_inputs.get_valid_char_check()
         if char_check == "yes" or char_check == "y":
             print ("You have to do it for the ascendancy!")
         else:
             print ("You can skip it!")
     else:
-        print (f"You have to do {q.quests_act2[quest_input]}.")
+        print (f"You have to do {quests_module.quests_act2[quest_input]}.")
     
 elif act_input == 3:
     pass
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 else:
     print("Act not implemented yet.")
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## napraviti loop nakon odgovora za odredjeni quest, da program pita jel to to ili ti treba jos neki odgovor!
-## negdje dodati: ako ti treba vise info, pogledaj na...related poe URL
-## ideja za split:
-## speed-run questovi / skip bosseva / ...
